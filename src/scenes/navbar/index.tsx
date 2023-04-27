e4f4b2423e9e69e0d2eeedc7769a07ab8bd6f3bb
Akindejoye <tydruhill@gmail.com>
2023-04-27 23:41:13 +0100

    App Logo

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PixIcon from '@mui/icons-material/Pix';
 import { Box, Typography, useTheme } from "@mui/material";
 import FlexBetween from '@/components/FlexBetween';
 
@@ -9,6 +10,7 @@ interface NavbarProps {
  
 const Navbar = () => {
   const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
 
   return ( 
     <FlexBetween 
@@ -16,7 +18,12 @@ const Navbar = () => {
       p='0.5rem 0rem' 
       color={palette.grey[300]}
     >
-      Navbar
+      <FlexBetween gap="0.75rem">
+        <PixIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          Finanseer
+        </Typography>
+      </FlexBetween>
     </FlexBetween>
    );
 }
