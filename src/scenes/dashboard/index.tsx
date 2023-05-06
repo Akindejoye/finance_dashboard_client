ac4e556476d70e2ab5d22e38b30ed41bed3e4cd8
Akindejoye <tydruhill@gmail.com>
2023-05-06 22:52:31 +0100

    Row2

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,6 +1,6 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./Row1";
-// import Row2 from "./Row2";
+import Row2 from "./Row2";
 // import Row3 from "./Row3";
 
 const gridTemplateLargeScreens = `
@@ -71,8 +71,8 @@ const Dashboard = () => {
       }
     >
       <Row1 />
-      {/* <Row2 />
-      <Row3 /> */}
+      <Row2 />
+      {/* <Row3 /> */}
     </Box>
   );
 };
